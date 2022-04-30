e7338174742d8084a8d73528e762a03e5164b23e
Tekipeps <tekena157@gmail.com>
2022-04-30 13:18:27 +0000

    Initialized app

---
+++ pages/report.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+// import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+export default function Report() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="Submit a report on an environmental challenge" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        
+      </main>
+      
+    </div>
+  )
+}

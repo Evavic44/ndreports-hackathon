6963ea5419469bf0783f927a4e84ab15e88209c1
Tekipeps <tekena157@gmail.com>
2022-04-30 14:41:34 +0000

    def

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import Head from 'next/head'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   return (
+    <>
+    <Head>
+    <Script src="https://widget.cloudinary.com/v2.0/global/all.js" type="text/javascript" />  
+    </Head>
     <ChakraProvider>
       <Component {...pageProps} />
-    </ChakraProvider>
+    </ChakraProvider></>
   )
 }
 
--- pages/report.js
+++ pages/report.js
@@ -1,16 +1,37 @@
 import Head from 'next/head'
 
+
+
 import {
+    Icon,
     FormControl,
     FormLabel,
     FormErrorMessage,
     Input,
+    Select,
     FormHelperText,
+    Flex,
+    Box,
+    Heading,
+    Button,
+    InputGroup,
+    InputLeftElement
 } from '@chakra-ui/react'
+import { useRef } from 'react'
 
-
-
+import { useForm } from 'react-hook-form';
 export default function Report() {
+    const inputRef = useRef();
+	const {
+		handleSubmit,
+		register,
+		setError,
+		control,
+		formState: { errors, isSubmitting },
+	} = useForm()
+
+
+
     return (
         <div>
             <Head>
@@ -21,14 +42,35 @@ export default function Report() {
 
 
             <main>
-
-                <form>
-                    <FormControl>
-                        <FormLabel htmlFor='email'>Email address</FormLabel>
-                        <Input id='email' type='email' />
-                        <FormHelperText>We'll never share your email.</FormHelperText>
-                    </FormControl>
-                </form>
+                {/* Address, Media */}
+                <Flex width="full" justifyContent="center">
+                    <Box p={2}>
+                        <Box textAlign="center">
+                            <Heading>Submit your report</Heading>
+                        </Box>
+                        <Box my={3} textAlign="left">
+                            <form>
+                                <FormControl>
+                                    <FormLabel htmlFor='name'>Name</FormLabel>
+                                    <Input id='name' type='name' placeholder='John Doe' />
+                                </FormControl>
+                                <FormControl>
+                                    <FormLabel htmlFor='email'>Email address</FormLabel>
+                                    <Input id='email' type='email' placeholder='johndoe@example.com' />
+                                </FormControl>
+                                <FormControl>
+                                    <FormLabel htmlFor='state'>State</FormLabel>
+                                    <Select id='state' placeholder='Select state'>
+                                        <option>Rivers</option>
+                                        <option>Delta</option>
+                                        <option>Abia</option>
+                                    </Select>
+                                </FormControl>
+                                {/* <CloudinaryUploadWidget /> */}
+                            </form>
+                        </Box>
+                    </Box>
+                </Flex>
             </main>
         </div>
     )

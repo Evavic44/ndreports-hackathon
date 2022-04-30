28b24f3e22968467b44db4d06dae2224df6846c0
Tekipeps <tekena157@gmail.com>
2022-04-30 13:25:11 +0000

    add chakra ui

--- pages/report.js
+++ pages/report.js
@@ -1,19 +1,35 @@
 import Head from 'next/head'
-// import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-export default function Report() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Submit a report on an environmental challenge" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main className={styles.main}>
-        
-      </main>
-      
-    </div>
-  )
+import {
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    Input,
+    FormHelperText,
+} from '@chakra-ui/react'
+
+
+
+export default function Report() {
+    return (
+        <div>
+            <Head>
+                <title>Create Next App</title>
+                <meta name="description" content="Submit a report on an environmental challenge" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+
+            <main>
+
+                <form>
+                    <FormControl>
+                        <FormLabel htmlFor='email'>Email address</FormLabel>
+                        <Input id='email' type='email' />
+                        <FormHelperText>We'll never share your email.</FormHelperText>
+                    </FormControl>
+                </form>
+            </main>
+        </div>
+    )
 }
